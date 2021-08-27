ebd85ff428cf8e704b6f86dd1fe45794436ad031
pankupanku <pankusingh232@gmail.com>
2021-08-27 18:45:22 +0530

    adding model serializer permission and view

---
+++ profile_api/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
+]
