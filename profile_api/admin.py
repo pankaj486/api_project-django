0b40b3390ca72415bf1883beb9dcf387d69f48f8
pankupanku <pankusingh232@gmail.com>
2021-08-28 15:44:31 +0530

    adding login authentication permissions serializers

--- profile_api/admin.py
+++ profile_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserProfileManager, UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 
 
 # admin.site.register(UserProfileManager)
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
--- profile_api/urls.py
+++ profile_api/urls.py
@@ -7,8 +7,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
